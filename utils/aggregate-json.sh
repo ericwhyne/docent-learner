c168f3011d5897e9793a7d6d4ac738563685108b
Eric Whyne <ericwhyne@gmail.com>
2014-09-27 04:35:31 -0400

    fixed aggregate script

--- utils/aggregate-json.sh
+++ utils/aggregate-json.sh
@@ -3,6 +3,7 @@ imagedir="/var/www/html/docent-learner/images/"
 outfilename=`date +"%Y-%m-%d"`-image-tags.json
 echo "[" > $outfilename
 cat $imagedir*.json | while read a; do echo $imagedir$a, >> $outfilename; done
+echo "]" >> $outfilename
 #sed -i '1s/^/\[/' $outfilename
-sed -i '$s/,$/\n\]/' $outfilename
+#sed -i '$s/,$/\n\]/' $outfilename
 echo "Filename: $outfilename"
