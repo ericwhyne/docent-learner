caaad2a3f692e77cc4ff03fd6db47507aa98b319
Eric Whyne <ericwhyne@gmail.com>
2014-10-31 13:06:00 -0400

    weird friday at work

--- src/textselect.py
+++ src/textselect.py
@@ -56,7 +56,7 @@ def application(environ, start_response):
     <input type="hidden" name="selected_text" id='input'>
     Notes:<textarea name="notes" cols="40" rows="5"></textarea><br>
     <br><br>
-    <center><input type="image" src="/docent-learner/static/submit.png" alt="Submit" width="100" height="100"></center>
+    <center><input type="submit" value="submit"></center>
     </form>
     <br> Random text file shown is:<br>  %s<br>
   """ % (currentfilename, currentfilename)
@@ -66,12 +66,12 @@ def application(environ, start_response):
     data += "\"" + key + "\":\"" + value + "\","
   data = data[:-1]
   data += "}\n"
-  textdisplay = "<table border=2 cellpadding=40><tr><td>" + text_to_tag + "</td></tr><tr><td><center>Selected text: <div id='show_input'></div></center></td></tr></table>"
+  textdisplay = "<table class='texttable' cellpadding=40><tr><td>" + text_to_tag + "</td></tr><tr><td><center>Selected text: <div id='show_input'></div></center></td></tr></table>"
   html = ""
   if (files_left == 1 and len(form_data) > 1) or currentfilename == "" :
     html = html_header + "All the text has been tagged!</html>"
   else:
-    html = html_header + "<table><tr><td>" + form + "</td><td>" + textdisplay + "</td></tr></table</html>"
+    html = html_header + "<table class='rounded'><tr><td>" + textdisplay + "</td><td>" + form + "</td></tr></table</html>"
 
   if len(form_data) > 1:
     datafilename = form_data.getvalue('tagged_text_filename') + ".json"
